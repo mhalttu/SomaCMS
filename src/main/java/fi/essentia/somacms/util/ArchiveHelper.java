7266e39a6c22303ff46bce45f5828c8ab8bbe354
Markus Halttunen <markus@smallgiantgames.com>
2013-04-08 22:43:17 +0300

    Sometimes an archive may first contain the file definition and the folder only afterwards so we'll need to sort them.

--- src/main/java/fi/essentia/somacms/util/ArchiveHelper.java
+++ src/main/java/fi/essentia/somacms/util/ArchiveHelper.java
@@ -6,11 +6,13 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.activation.MimetypesFileTypeMap;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -21,35 +23,105 @@ import java.util.zip.ZipInputStream;
 public class ArchiveHelper {
     @Autowired private DocumentManager documentManager;
 
-    public void storeDocuments(Long rootId, byte[] bytes) throws IOException {
+    public void storeDocuments(TreeDocument targetFolder, byte[] bytes) throws IOException {
+        List<DocumentEntry> entries = new ArrayList<DocumentEntry>();
         ZipInputStream in = new ZipInputStream(new ByteArrayInputStream(bytes));
         ZipEntry zipEntry = in.getNextEntry();
         while(zipEntry!=null){
             File file = new File(zipEntry.getName());
-            String parentPath = file.getParent();
-            TreeDocument parent;
-            if (parentPath == null) {
-                parent = documentManager.documentById(rootId);
-            } else {
-                parent = documentManager.documentFromPath(parentPath);
-            }
-            if (parent == null) {
-                throw new NullPointerException("Failed to find the parent folder for " + zipEntry.getName());
-            }
-
             if (zipEntry.isDirectory()) {
-                if (parent.childByName(file.getName()) == null) {
-                    documentManager.createFolder(parent.getId(), file.getName());
-                }
+                entries.add(new DocumentEntry(true, file.getParent(), file.getName(), null));
             } else {
                 ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
                 IOUtils.copy(in, out);
                 out.close();
-                documentManager.storeDocument(parent.getId(), file.getName(), out.toByteArray());
+                entries.add(new DocumentEntry(false, file.getParent(), file.getName(), out.toByteArray()));
             }
             zipEntry = in.getNextEntry();
         }
         in.closeEntry();
         in.close();
+
+        Collections.sort(entries);
+        for (DocumentEntry entry : entries) {
+            TreeDocument parent;
+            if (entry.path == null) {
+                parent = documentManager.documentById(targetFolder.getId());
+                if (parent == null) {
+                    throw new RuntimeException("Failed to find the target folder");
+                }
+            } else {
+                parent = findParent(targetFolder, entry.path);
+            }
+            if (parent == null) {
+                System.out.println("parent = " + null);
+            } else {
+                System.out.println("parent.getPath() = " + parent.getPath());
+            }
+            System.out.println("entry.path = " + entry.path);
+            if (parent == null) {
+                throw new NullPointerException("Failed to find the parent folder for " + entry.name);
+            }
+
+            if (entry.folder) {
+                if (parent.childByName(entry.name) == null) {
+                    documentManager.createFolder(parent.getId(), entry.name);
+                }
+            } else {
+                documentManager.storeDocument(parent.getId(), entry.name, entry.data);
+            }
+        }
+    }
+
+    private TreeDocument findParent(TreeDocument targetFolder, String filePath) {
+        if (targetFolder.isRoot()) {
+            return documentManager.documentFromPath(filePath);
+        } else {
+            String rootPath = targetFolder.getPath().substring(1);
+            return documentManager.documentFromPath(rootPath + filePath);
+        }
+    }
+
+    /**
+     * Sometimes an archive may first contain the file definition and the folder only afterwards so we'll need to
+     * sort them.
+     */
+    private static class DocumentEntry implements Comparable<DocumentEntry> {
+        private boolean folder;
+        private String path;
+        private String name;
+        private byte[] data;
+
+        private DocumentEntry(boolean folder, String path, String name, byte[] data) {
+            this.folder = folder;
+            this.path = path;
+            this.name = name;
+            this.data = data;
+        }
+
+        @Override
+        public int compareTo(DocumentEntry other) {
+            if (folder) {
+                if (!other.folder) {
+                    return -1;
+                }
+            } else if (other.folder) {
+                return 1;
+            }
+
+            int pathDiff = pathLength() - other.pathLength();
+            if (pathDiff != 0) {
+                return pathDiff;
+            }
+
+            return name.compareTo(other.name);
+        }
+
+        private int pathLength() {
+            if (path == null) {
+                return 0;
+            }
+            return path.split("/").length;
+        }
     }
 }
