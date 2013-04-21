2e901f52c3d635d4dd73532d3507e708d3128f96
Markus Halttunen <markus@smallgiantgames.com>
2013-04-21 14:49:07 +0300

    Interface that exposes only read operations to the DataDao so that we don't even accidentally use it to bypass DocumentManager.

--- src/main/java/fi/essentia/somacms/dao/DataDao.java
+++ src/main/java/fi/essentia/somacms/dao/DataDao.java
@@ -3,8 +3,7 @@ package fi.essentia.somacms.dao;
 /**
  * Stores and loads the byte-based data (i.e. the contents) of the documents
  */
-public interface DataDao {
+public interface DataDao extends ReadOnlyDataDao {
     void insertData(long documentId, byte[] data);
     void updateData(long documentId, byte[] data);
-    byte[] loadData(long documentId);
 }
